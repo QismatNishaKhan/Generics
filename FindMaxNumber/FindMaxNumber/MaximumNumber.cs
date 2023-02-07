a375855418c0623fe06ec1a8f03ef69fd608e2f5
qismat <qismatnisha6243@gmail.com>
2023-02-07 21:22:47 +0530

    [qismat] Add :UC3-Refactor1-GenericClass

--- FindMaxNumber/FindMaxNumber/MaximumNumber.cs
+++ FindMaxNumber/FindMaxNumber/MaximumNumber.cs
@@ -7,26 +7,37 @@ using System.Threading.Tasks;
 namespace FindMaxNumber
 {
 
-    public class MaximumNumber
+    public class MaximumNumber<T> where T : IComparable<T>
     {
-        public static string MaximumStringNum(string firstValue, string secondValue, string thirdValue)
+        private T first_Value;
+        private T second_Value;
+        private T third_Value;
+        public MaximumNumber(T first_Value, T second_Value, T third_Value)
         {
-            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
-
-            {
-                return firstValue;
-            }
-            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
-            {
-                return secondValue;
-            }
-            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
-            {
-                return thirdValue;
-            }
-            return firstValue;
+            this.first_Value = first_Value;
+            this.second_Value = second_Value;
+            this.third_Value = third_Value;
         }
+        public static T GetMaximum(T first_Value, T second_Value, T third_Value)
+        {
+            if (first_Value.CompareTo(second_Value) > 0 && first_Value.CompareTo(third_Value) > 0)
+            {
+                return first_Value;
+            }
+            else if (second_Value.CompareTo(first_Value) > 0 && second_Value.CompareTo(third_Value) > 0)
+            {
+                return second_Value;
+            }
+            else if (third_Value.CompareTo(first_Value) > 0 && third_Value.CompareTo(second_Value) > 0)
+            {
+                return third_Value;
+            }
+            else
+            {
+                throw new Exception("Values are same");
+            }
 
+        }
 
     }
 }
